641714aa2231c8fceb74dee03df7e2750b4b0a33
David <19843400@qq.com>
2019-11-14 08:46:35 +0800

    去掉一些不必要的依赖

--- app/src/main/java/com/drgou/imagecommon/MainActivity.java
+++ app/src/main/java/com/drgou/imagecommon/MainActivity.java
@@ -1,21 +1,15 @@
 package com.drgou.imagecommon;
 
-import android.Manifest;
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.drgou.imagecommonlibrary.ImgCommon;
-import com.tbruyelle.rxpermissions2.RxPermissions;
-
-import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
-    final RxPermissions rxPermissions = new RxPermissions(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.test_iv);
 
-        ImgCommon.setImg(imageView, "http://pic26.nipic.com/20121229/9252150_101107077359_2.jpg");
+        ImgCommon.setImg(imageView, "http://images.baixingliangfan.cn/homeFloor/20180407/20180407181423_15.jpg");
 
-//        rxPermissions
-//                .request(Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                .subscribe(new Consumer<Boolean>() {
-//                    @Override
-//                    public void accept(Boolean granted) throws Exception {
-//                        if (granted) { // Always true pre-M
-//                            ImgCommon.setImg(imageView, "http://pic26.nipic.com/20121229/9252150_101107077359_2.jpg");
-//                        } else {
-//                            // Oups permission denied
-//                            Toast.makeText(MainActivity.this,"没有权限!",Toast.LENGTH_SHORT).show();
-//                        }
-//                    }
-//                });
 
     }
 }
